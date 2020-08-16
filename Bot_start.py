dfdd3405853c50ffa7871bebe605be65df7ed524
xolodutra <xolodutra@gmai.com>
2020-08-16 17:09:21 +0300

    добавил в функцию со смайлами возможность привязки смайла к конкретному пользователю

--- Bot_start.py
+++ Bot_start.py
@@ -9,8 +9,31 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
 
-logging.basicConfig(filename='bot.log',
-                    level=logging.INFO)
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+# Создадим функцию обработки события в телеге:
+
+def greet_user(update, context):
+    print("Вызыван /start")
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f"Ну здорова, отец {context.user_data['emoji']}!")
+
+
+# Создадим функцию __inpu_bot__ где и будет происходить вся логика работы бота:
+
+def talk_to_me(update, context):
+    context.user_data['emoji'] = get_smile(context.user_data)
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(f"{user_text} {context.user_data['emoji']}")
+
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data['emoji']
+
 
 # Создаём функцию send_cat_picture для рандомной выдачи картинки с котиком
 
@@ -52,25 +75,6 @@ def guess_number(update, context):
         message = "Введите целое число"
     update.message.reply_text(message)
 
-# Создадим функцию обработки события в телеге:
-
-
-def greet_user(update, context):
-    print("Вызыван /start")
-    smile = get_smile()
-    update.message.reply_text(f"Ну здорова, отец {smile}!")
-
-# Создадим функцию __inpu_bot__ где и будет происходить вся логика работы бота:
-
-def talk_to_me(update, context):
-    smile = get_smile()
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(f"{user_text} {smile}")
-
-def get_smile():
-    smile = choice(settings.USER_EMOJI)
-    return emojize(smile, use_aliases=True)
 
 
 def inpu_bot():
