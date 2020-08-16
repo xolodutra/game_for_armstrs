017bba7bcd7c760b7db0724f28e363481f2c764f
xolodutra <xolodutra@gmai.com>
2020-08-16 21:27:15 +0300

    Добавил обработку кнопок

--- Bot_start.py
+++ Bot_start.py
@@ -4,7 +4,7 @@
 from emoji import emojize
 from glob import glob
 import logging
-from telegram import ReplayKeyboardMarkup
+from telegram import ReplyKeyboardMarkup
 from random import choice, randint
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -13,12 +13,16 @@ import settings
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 
-# Создадим функцию обработки события в телеге:
+# Создадим функцию обработки события /start в телеге:
 
 def greet_user(update, context):
     print("Вызыван /start")
     context.user_data['emoji'] = get_smile(context.user_data)
-    update.message.reply_text(f"Ну здорова, отец {context.user_data['emoji']}!")
+    my_keyboard = ReplyKeyboardMarkup([['Очень нужен котик']])
+    update.message.reply_text(
+        f"Ну здорова, отец {context.user_data['emoji']}!",
+        reply_markup=my_keyboard
+        )
 
 
 # Создадим функцию __inpu_bot__ где и будет происходить вся логика работы бота:
@@ -53,15 +57,17 @@ def send_cat_picture(update, context):
 
 # Создаем функцию guess_number для игры с пользователем в числа
 
+
 def play_random_numbers(user_number):
     bot_number = randint(user_number-10, user_number+10)
     if user_number > bot_number:
         message = f"Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!"
     elif user_number == bot_number:
         message = f"Ты загадал {user_number}, я загадал {bot_number}, ничья!"
-    else: 
+    else:
         message = f"Ты загадал {user_number}, я загадал {bot_number}, я выиграл!"
     return message
+
 
 def guess_number(update, context):
     print(context.args)
@@ -77,20 +83,19 @@ def guess_number(update, context):
     update.message.reply_text(message)
 
 
-
 def inpu_bot():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("cat", send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.regex('^(Очень нужен котик)$'), send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
 # Запрашиваем у телеги есть ли новые сообщения?
     logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
-
 
 if __name__=="__main__":
     inpu_bot()
